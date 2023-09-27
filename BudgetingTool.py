4a8b20bcacf5c4853375505ef1c88702e69a7002
Stephen <146043665+StephenOktagon@users.noreply.github.com>
2023-09-27 22:17:31 +0100

    Update BudgetingTool.py
    - Added budget save/load mechanism

--- BudgetingTool.py
+++ BudgetingTool.py
@@ -1,3 +1,5 @@
+import json
+
 class BudgetingTool:
     def __init__(self):
         self.income = 0
@@ -36,11 +38,38 @@ class BudgetingTool:
         budget = self.calculate_budget()
         print(f"Remaining Budget: ${budget:.2f}")
 
+    def save_budget(self, filename):
+        data = {
+            'income': self.income,
+            'expenses': self.expenses
+        }
+        with open(filename, 'w') as file:
+            json.dump(data, file)
+        print(f"Budget data saved to {filename}")
+
+    def load_budget(self, filename):
+        try:
+            with open(filename, 'r') as file:
+                data = json.load(file)
+                self.income = data['income']
+                self.expenses = data['expenses']
+            print(f"Budget data loaded from {filename}")
+        except FileNotFoundError:
+            print(f"File '{filename}' not found. Starting with a new budget.")
+
     def run(self):
         print("Welcome to the Budgeting Tool!")
+        budget_data_file = "budget_data.json"  # Change the filename as needed
+
+        # Load existing budget data if available
+        self.load_budget(budget_data_file)
+
         self.get_income()
         self.get_expenses()
         self.display_budget()
+
+        # Save the budget data
+        self.save_budget(budget_data_file)
 
 
 if __name__ == "__main__":
